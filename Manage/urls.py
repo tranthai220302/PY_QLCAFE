08889681bc25387f46e5cd3739d78b1503f6c6d8
LAPTOP-M0F2IN9G\this PC <nguyenthanhlich112002@gmail.com>
2023-05-15 18:36:26 +0700

    15-5

--- Manage/urls.py
+++ Manage/urls.py
@@ -19,4 +19,23 @@ urlpatterns = [
     path('pay', views.pay, name="pay"),
     path('purchaseorder', views.purchaseorder, name="purchaseorder"),
     path('myaccount', views.myaccount, name="myaccount"),
+    # login
+    # path('login', views.login_view, name='login'),
+    # path('checklogin', views.dangnhap, name='checklogin'),
+    # path('logout', views.my_logout, name='logout'),
+    # addDish
+    path('home/', views.home, name='home'),
+    path('manage_menu<int:id>/<int:id_page>',
+         views.manage_menu, name='manage_menu'),
+    path('delete_post', views.delete_post, name='delete_post'),
+    path('edit_dish', views.edit_dish, name='edit_dish'),
+    path('create_dish', views.create_dish, name='create_dish'),
+    # ManageCustomer
+    path('manage_Customer<int:id_page>',
+         views.manage_Customer, name='manage_Customer'),
+    path('delete_customer<int:id>/<int:id_page>',
+         views.delete_customer, name='delete_customer'),
+
+    path('success_order<int:id>', views.success_order, name='success_order'),
+    path('cartnhanvien', views.listOrder, name='cartnhanvien'),
 ]
--- Manage/views.py
+++ Manage/views.py
@@ -1,25 +1,39 @@
-from datetime import date
-from pyexpat.errors import messages
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout, decorators
 import json
-from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from rest_framework.decorators import api_view
-from Shop.models import Order, Dish, Cart, Customer, User
-
+from Shop.models import Order, Dish, Cart, Customer, Menu, Employee
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+import math
+import random
+from django.views.decorators.csrf import csrf_exempt
+import os
+import shutil
+from django.conf import settings
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def Home_page(request):
     queryset = Dish.objects.filter()
+    nhanvien = Employee.objects.filter()
     if not request.user.is_authenticated:
-        return render(request, 'index.html', {'Dish': queryset})
+        return render(request, 'index.html', {'Dish': queryset, 'nhanvien': nhanvien})
     else:
         user = request.user
         if user.is_staff == True:
-            return render(request, 'index.html', {'Dish': queryset})
+            return render(request, 'index.html', {'Dish': queryset, 'nhanvien': nhanvien})
         else:
-            return render(request, 'formLogin.html')
+            list_menu = Menu.objects.filter()
+            nhanvien = Employee.objects.filter(employee_id=user.id)
+            return render(request, 'Manage/home.html', {'list_menu': list_menu, 'my_user': user, 'nhanvien': nhanvien})
 
 
 @decorators.login_required(login_url='/formlogin')
@@ -38,19 +52,27 @@ def my_login(request):
         if not user.is_authenticated:
             user_name = request.POST.get('username')
             pass_word = request.POST.get('password')
-            my_user = authenticate(
-                request, username=user_name, password=pass_word)
             print(user_name)
             print(pass_word)
+            my_user = authenticate(
+                request, username=user_name, password=pass_word)
             if my_user is None:
                 return render(request, 'formLogin.html')
             login(request, my_user)
-            print(user.is_staff)
+            print(my_user.is_staff)
+            if my_user.is_staff == True:
+                return render(request, 'index.html', {'Dish': queryset})
+            else:
+                list_menu = Menu.objects.filter()
+                nhanvien = Employee.objects.filter(employee_id=user.id)
+                return render(request, 'Manage/home.html', {'list_menu': list_menu, 'my_user': user, 'nhanvien': nhanvien})
+        else:
             if user.is_staff == True:
                 return render(request, 'index.html', {'Dish': queryset})
             else:
-                return render(request, 'formLogin.html')
-        return render(request, 'index.html', {'Dish': queryset})
+                list_menu = Menu.objects.filter()
+                nhanvien = Employee.objects.filter(employee_id=user.id)
+                return render(request, 'Manage/home.html', {'list_menu': list_menu, 'my_user': user, 'nhanvien': nhanvien})
 
 
 @decorators.login_required(login_url='/formlogin')
@@ -181,3 +203,146 @@ def testimonial(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def home(request):
+    user = request.user
+    my_user = User.objects.get(id=user.id)
+    nhanvien = Employee.objects.get(employee_id=user.id)
+    nhanvien = Employee.objects.get(employee_id=user.id)
+    list_menu = Menu.objects.filter()
+    return render(request, 'Manage/home.html', {'list_menu': list_menu, 'my_user': my_user, 'nhanvien': nhanvien})
+
+
+def manage_menu(request, id, id_page):
+    user = request.user
+    my_user = User.objects.get(id=user.id)
+    nhanvien = Employee.objects.get(employee_id=user.id)
+    print(id)
+    list_Dish = Dish.objects.filter(menu_id=id).order_by('-id')
+    num_page = math.ceil(list_Dish.count()/6)
+    arr = [i+1 for i in range(num_page)]
+    paginator = Paginator(list_Dish, 6)  # phân trang với 5 items/trang
+    try:
+        list_Dish = paginator.page(id_page)
+    except PageNotAnInteger:
+        list_Dish = paginator.page(1)
+    except EmptyPage:
+        list_Dish = paginator.page(paginator.num_pages)
+    menu = Menu.objects.get(id=id)
+    return render(request, 'Manage/add.html', {'list_Dish': list_Dish, 'menu': menu, 'num_page': arr, 'id_page': id_page, 'my_user': my_user, 'nhanvien': nhanvien})
+
+
+@csrf_exempt
+def delete_post(request):
+
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        Dish_delete = Dish.objects.get(id=post_id)
+        Dish_delete.delete()
+        data = {'deleted': True}
+    else:
+        data = {'deleted': False}
+    return JsonResponse(data)
+
+
+@csrf_exempt
+def create_dish(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        price = request.POST['price']
+        image = request.FILES['image']
+        menu_id = request.POST['menu_id']
+        # Lưu tập tin vào thư mục "static/media/images"
+        image_name = os.path.join(
+            settings.MEDIA_ROOT, 'media', request.FILES['image'].name)
+        with open(image_name, 'wb') as f:
+            f.write(request.FILES['image'].read())
+        dish = Dish(menu_id=menu_id, name=name, price=price,
+                    image='media/' + request.FILES['image'].name)
+        dish.save()
+        image_url = 'media/' + request.FILES['image'].name
+        url = "{% static '"+image_url+"' %}"
+
+        return JsonResponse(
+            {
+                'success':  True,
+                'id': dish.id,
+                'image_url':  image_url,
+                'name': dish.name,
+                'price': dish.price
+            }
+        )
+    else:
+        return HttpResponse('Request method is not POST.')
+
+
+@csrf_exempt
+def edit_dish(request):
+    if request.method == 'POST':
+        dish_id = request.POST.get('dish_id')
+        name = request.POST['name']
+        price = request.POST['price']
+        # Lưu tập tin vào thư mục "static/media/images"
+        image_name = os.path.join(
+            settings.MEDIA_ROOT, 'media', request.FILES['image'].name)
+        with open(image_name, 'wb') as f:
+            f.write(request.FILES['image'].read())
+        dish = Dish.objects.get(id=dish_id)
+        dish.name = name
+        dish.price = price
+        dish.image = 'media/' + request.FILES['image'].name
+        dish.save()
+        return JsonResponse({'success':  True})
+    return HttpResponse('Request method is not POST.')
+
+
+# Manage Customer
+
+def manage_Customer(request, id_page):
+    user = request.user
+    my_user = User.objects.get(id=user.id)
+    nhanvien = Employee.objects.get(employee_id=user.id)
+    list_Customer = Customer.objects.filter()
+    num_page = math.ceil(list_Customer.count()/4)
+    arr = [i+1 for i in range(num_page)]
+    paginator = Paginator(list_Customer, 4)  # phân trang với 5 items/trang
+    try:
+        list_Customer = paginator.page(id_page)
+    except PageNotAnInteger:
+        list_Customer = paginator.page(1)
+    except EmptyPage:
+        list_Customer = paginator.page(paginator.num_pages)
+    return render(request, 'Manage/manageCutomer.html', {'list_Customer': list_Customer, 'num_page': arr, 'id_page': id_page, 'my_user': my_user, 'nhanvien': nhanvien})
+
+
+def delete_customer(request, id, id_page):
+    user = request.user
+    my_user = User.objects.get(id=user.id)
+    nhanvien = Employee.objects.get(employee_id=user.id)
+    customer_delete = Customer.objects.get(id=id)
+    customer_delete.delete()
+    list_Customer = Customer.objects.filter()
+    num_page = math.ceil(list_Customer.count()/4)
+    arr = [i+1 for i in range(num_page)]
+    paginator = Paginator(list_Customer, 4)  # phân trang với 5 items/trang
+    try:
+        list_Customer = paginator.page(id_page)
+    except PageNotAnInteger:
+        list_Customer = paginator.page(1)
+    except EmptyPage:
+        list_Customer = paginator.page(paginator.num_pages)
+    return render(request, 'Manage/manageCutomer.html', {'list_Customer': list_Customer, 'num_page': arr, 'id_page': id_page, 'my_user': my_user, 'nhanvien': nhanvien})
+
+
+def success_order(request, id):
+    order_update = Cart.objects.get(id=id)
+    order_update.status = 'receiced'
+    order_update.save()
+    listOrder = Cart.objects.filter(status='delivery')
+    return render(request, 'Manage/cartnhanvien.html', {'listOrder': listOrder})
+
+
+def listOrder(request):
+    listOrder = Cart.objects.filter(status='delivery')
+    return render(request, 'Manage/cartnhanvien.html', {'listOrder': listOrder})
